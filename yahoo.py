2496d65d4586fa782223b62c184b1171bd4262a7
mark rosetta <marked@gmail.com>
2019-11-24 20:43:38 -0600

    Fix for unicode crashing logging. PR #20 Issue #13

--- yahoo.py
+++ yahoo.py
@@ -930,7 +930,7 @@ if __name__ == "__main__":
             args.polls = args.attachments = args.members = args.topics = args.raw = True
 
     with Mkchdir(args.group, sanitize=False):
-        log_file_handler = logging.FileHandler('archive.log')
+        log_file_handler = logging.FileHandler('archive.log','w','utf-8')
         log_file_handler.setFormatter(log_formatter)
         root_logger.addHandler(log_file_handler)
 
